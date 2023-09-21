1bcc22e4155b9e5ad7911718428d75a29e964860
MTsuyama-git <miyabi-yamabiko@hotmail.co.jp>
2023-09-21 23:35:45 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,8 @@
-use bitfield::bitfield;
-use bitfield_struct::bitfield as bitfield_struct;
+use bitfield_struct::bitfield;
 
-#[macro_use]
-extern crate bitflags;
-extern crate bitfield;
 extern crate bitfield_struct;
 
-bitfield! {
-    #[derive (Clone, Copy, Debug)]
-    struct MyRegField(u32);
-    lsb, set_lsb: 1, 0;
-    p0,  set_p0: 10, 0;
-/*     p1,  set_p1: 10, 11; */
-    p2,  set_p2: 10, 0;
-    /* msb, set_msb: 1, 31; */
-}
-
-union  MyRegFieldUnion{
-    field: MyRegField,
-    raw : u32,
-}
-
-bitflags! {
-    struct MyField: u32 {
-        const LSB     = 0b00000000_00000000_00000000_00000001;
-        const P0      = 0b00000000_00000000_00000111_11111110;
-        const P1      = 0b00000000_00011111_11111000_00000000;
-        const P2      = 0b01111111_11100000_00000000_00000000;
-        const MSB     = 0b10000000_00000000_00000000_00000000;
-    }
-}
-
-#[bitfield_struct(u32)]
+#[bitfield(u32)]
 struct MyRegStructRegField {
     #[bits(1)]
     lsb: u32,
@@ -52,17 +23,7 @@ union MyRegStruct {
 
 fn main() {
     unsafe {
-        let mut my_reg = MyRegFieldUnion { raw: 0 };
-
-        println!("my_reg: 0x{:08x}", my_reg.raw);
-        my_reg.field.set_lsb(1);
-        println!("my_reg: 0x{:08x}", my_reg.raw);
-        my_reg.raw = 0;
-        my_reg.field.set_p2(1023);
-        println!("my_reg: 0x{:08x}", my_reg.raw);
-        println!("Hello, world!");
-
-    let mut my_reg_struct: MyRegStruct = MyRegStruct { raw: 0 };
+        let mut my_reg_struct: MyRegStruct = MyRegStruct { raw: 0 };
         println!("my_reg_struct: {}", my_reg_struct.raw);
         my_reg_struct.field.set_lsb(1);
         println!("my_reg_struct: {}", my_reg_struct.raw);
